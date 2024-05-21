b9c6bc5e2e8af4ed8f41a51be88bd174c02c515f
Blaise Sebagabo <ntwariblaze@gmail.com>
2024-05-21 15:20:03 -0600

    users-permissions plugin added

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+const { env } = require("@strapi/utils");
+
+module.exports = () => ({
+  "users-permissions": {
+    config: {
+      jwt: {
+        secret: env("JWT_SECRET"),
+      },
+    },
+  },
+});
